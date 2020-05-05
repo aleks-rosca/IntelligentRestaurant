de5487c29b1567a7979474198ab641240ddf6438
Eric Volmer <eric.volmer@hotmail.com>
2020-05-06 01:40:37 +0200

    Initial commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Food()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Drinks()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Meat()
+        {
+            return View();
+        }
+
+        public ActionResult Soup()
+        {
+            return View();
+        }
+
+        public ActionResult Snacks()
+        {
+            return View();
+        }
+
+        public ActionResult Dessert()
+        {
+            return View();
+        }
+    }
+}
